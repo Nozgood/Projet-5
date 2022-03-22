b27e1867346dbf33fe5acc5caea47672239c7263
Nowfeel <n.safi@groupecerise.fr>
2022-03-22 15:11:41 +0100

    affichage des 8 premiers kanaps git status

--- front/js/script.js
+++ front/js/script.js
@@ -1,10 +1,5 @@
 // creation de variables pointant les infos des items
-let kanapImg = document.querySelector('article img');
-let kanapName = document.querySelector('h3.productName');
-let kanapDescription = document.querySelector('p.productDescription');
 let kanapSection = document.getElementById('items');
-let kanapItem = document.querySelector('section a');
-let kanapCopy = kanapItem.cloneNode(true);
 
 // récupération et affichage des données de l'API 
 
@@ -17,10 +12,25 @@ fetch('http://localhost:3000/api/products')
     .then(function(value) {
         console.log(value);
         for (let i = 0; i < value.length; i++) {
-            kanapName.innerHTML = value[i].name,
-            kanapItem.setAttribute('href', 'product.html?id=' + value[0]._id);
-            kanapDescription.innerHTML = value[i].description;
-            kanapImg.setAttribute('src', value[i].imageUrl);
-            kanapImg.setAttribute('alt', value[i].altTxt);
+            // creation des balises 
+            let itemLien = document.createElement('a');
+            kanapSection.appendChild(itemLien);
+            let itemArticle = document.createElement('article');
+            let itemImg = document.createElement('img');
+            let itemTitle = document.createElement('h3');
+            let itemParagraph = document.createElement('p');
+
+            // affiliation 
+            itemLien.appendChild(itemArticle);
+            itemArticle.appendChild(itemImg);
+            itemArticle.appendChild(itemTitle);
+            itemArticle.appendChild(itemParagraph);
+
+            // ajout du contenu 
+            itemTitle.innerHTML = value[i].name;
+            itemParagraph.innerHTML = value[i].description 
+            itemImg.setAttribute('src', value[i].imageUrl);
+            itemImg.setAttribute('alt', value[i].altTxt);
+            itemLien.setAttribute('href', 'product.html?id=' + value[i]._id)
         }
     });
