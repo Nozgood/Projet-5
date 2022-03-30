0892a7aa60d7d17cb3b721fbb11db2f12892fc2e
Nowfeel <n.safi@groupecerise.fr>
2022-03-30 12:22:44 +0200

    edit comment (in english please) + fetch double then + start localstorage basket

--- front/js/product.js
+++ front/js/product.js
@@ -5,6 +5,8 @@ let itemImg = document.querySelector('article div img');
 let itemPrice = document.getElementById('price');
 let itemDesc = document.getElementById('description');
 let itemColors = document.getElementById('colors');
+let itemQuantity = document.getElementById('quantity')
+let addBasket = document.getElementById('addToCart')
 
 // id recup to display infos 
 let url = new URL(window.location.href);
@@ -21,7 +23,6 @@ function fetchi() {
     })
     .then
     (function(value) {
-        console.log(value);
         title.innerHTML = value.name;
         itemName.innerHTML = value.name;
         itemImg.setAttribute('src', value.imageUrl)
@@ -39,15 +40,79 @@ function fetchi() {
 // display infos according to the id 
 fetchi();
 
+// add to basket 
+// init variables 
+
+
+let quantityBasket = localStorage.setItem('quantity', '');
+
+
+// store item datas to display on the basket page 
+addBasket.addEventListener('click', function() {
+    let selectedColor = itemColors.options[itemColors.selectedIndex].text;
+    let selectedQty = itemQuantity.value;
+    let idBasket = localStorage.setItem('id', id);
+    let colorBasket = localStorage.setItem('color', selectedColor);
+    let quantityBasket = localStorage.setItem('quantity', selectedQty);
+    console.log(localStorage);
+    console.log(selectedColor)
+;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // if datas needed
-fetch('http://localhost:3000/api/products/')
-    .then
-    (function (res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then
-    (function (value) {
-        console.log(value);
-    })
+// fetch('http://localhost:3000/api/products/')
+//     .then
+//     (function (res) {
+//         if (res.ok) {
+//             return res.json();
+//         }
+//     })
+//     .then
+//     (function (value) {
+//         console.log(value);
+//     })
